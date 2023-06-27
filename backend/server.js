c4b427644bd682d3b0a12cdaa97b491d10ccae88
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-06-27 22:37:54 +0200

    Add routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
-const express = require("express");
+require('dotenv').config()
+
+const express = require("express")
+const workoutRoutes = require('./routes/workouts')
 
 // express app
-const app = express();
+const app = express()
 
-// listen for request
-app.listen(4000, () => {
-    console.log("listening on port 4000")
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
 
+// routes
+app.use(workoutRoutes)
+
+// listen for request
+app.listen(process.env.PORT, () => {
+    console.log("listening on port 4000")
+});
