0fc4831212083e45e02636942b6a0477870ec310
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-06-28 22:13:54 +0200

    Install mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express")
-const workoutRoutes = require('./routes/workouts')
+const express = require("express");
+const workoutRoutes = require('./routes/workouts');
+const mongoose = require("mongoose");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    next();
+});
 
 // routes
-app.use(workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-// listen for request
-app.listen(process.env.PORT, () => {
-    console.log("listening on port 4000")
-});
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for request
+        app.listen(process.env.PORT, () => {
+            console.log("listening on port 4000");
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
