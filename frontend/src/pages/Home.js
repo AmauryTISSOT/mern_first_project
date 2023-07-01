9c0ab82f476595c803cb63198435c2dfcfbe0f96
Amaury Tissot <104317431+AmauryTISSOT@users.noreply.github.com>
2023-07-01 16:28:51 +0200

    Add fetch API

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
+// components
+import WorkoutsDetails from '../components/WorkoutsDetails';
 
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts');
+            const json = await response.json();
+
+            if (response.ok) {
+                setWorkouts(json);
+            }
+        };
+
+        fetchWorkouts();
+    }, []);
+
     return (
-        <h1 className='Home'>Home</h1>
+        <div className='Home'>
+            {workouts && workouts.map((workout) => (
+                <WorkoutsDetails key={workout._id} workout={workout} />
+            ))}
+        </div>
     );
 };
 
